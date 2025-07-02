9053a7b13f324d364047e1de81c0a4bf1c289717
SEYED <1mr.rooter3@gmail.com>
2025-07-02 05:22:45 +0330

    custom style  for container & add styles

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: "0.5rem",
+      screens: {
+        customSm: "566px",
+        md: "728px",
+        lg: "952px",
+      },
+    },
+  },
+};
