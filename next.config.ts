980d231b397cd3751b4e355b9da8f62a7ba1d010
SEYED <1mr.rooter3@gmail.com>
2025-08-02 16:39:38 +0330

    fix deploy err

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://mpttools.co/media/**')],
-  },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
   },
 };
 
